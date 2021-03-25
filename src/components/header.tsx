8b2ff061a9305352aa2da4a5bd25e7feef392d71
Eyal <elevyg91@gmail.com>
2021-03-25 12:36:42 -0300

    :wrench:

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,16 +2,19 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
-const Header = ({ siteTitle }) => (
+
+
+const Header = ({ siteTitle }: {siteTitle: string}) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `white`,
       marginBottom: `1.45rem`,
     }}
   >
     <div
       style={{
         margin: `0 auto`,
+        
         maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
       }}
@@ -20,7 +23,7 @@ const Header = ({ siteTitle }) => (
         <Link
           to="/"
           style={{
-            color: `white`,
+            color: `red`,
             textDecoration: `none`,
           }}
         >
