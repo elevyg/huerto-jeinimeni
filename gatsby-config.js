03119e4c1b2988990d39557ca0b207112e18057a
Eyal <elevyg91@gmail.com>
2021-03-25 12:40:05 -0300

    :wrench:

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     `gatsby-plugin-emotion`,
+    'gatsby-plugin-root-import',
     {
       resolve: "gatsby-plugin-eslint",
       options: {
