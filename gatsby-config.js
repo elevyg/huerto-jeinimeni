6587162bfac1ea36a79cf6a4a19b8f9b6ac4f610
Eyal <elevyg91@gmail.com>
2021-03-26 18:20:24 -0300

    :wrench:

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
+  pathPrefix: "/huerto-jeinimeni",
   siteMetadata: {
-    pathPrefix: "/huerto-jeinimeni",
     title: `Huerto Jeinimeni`,
     description: `Somos un huerto Biointensivo, orgánico y sustentable ubicado en Chile Chico, Patagonia Chilena.`,
     author: `@eyalll`,
